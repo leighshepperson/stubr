05187d55da3fa5552c0f4af51ecfe2bfdbc7484e
leighshepperson <leigh.shepperson@icloud.com>
2016-09-13 14:07:12 +0100

    refactored

--- lib/stubr.ex
+++ lib/stubr.ex
@@ -1,22 +1,21 @@
 defmodule Stubr do
   @name_generator Application.get_env(:stubr, :name_generator)
 
-  defmacro stub(functions) do
+  defmacro stub(function_reps) do
+    content = function_reps |> create_content
 
-     body = functions |> create_body
+    stub_name = @name_generator.generate()
 
-     stub_name = @name_generator.generate()
-
-    {_, stub, _, _} = Module.create(:"Elixir.#{stub_name}", body, Macro.Env.location(__ENV__))
+    {_, stub, _, _} = Module.create(:"Elixir.#{stub_name}", content, Macro.Env.location(__ENV__))
 
     stub
   end
 
-  defp create_body(functions) do
-    functions_with_args = functions |> get_args
+  defp create_content(function_reps) do
+    function_reps = function_reps |> put_args
 
-    quote bind_quoted: [functions_with_args: Macro.escape(functions_with_args)] do
-      for {name, impl, args} <- functions_with_args do
+    quote bind_quoted: [function_reps: Macro.escape(function_reps)] do
+      for {name, impl, args} <- function_reps do
         def unquote(name)(unquote_splicing(args)) do
           unquote(impl).(unquote_splicing(args))
         end
@@ -24,10 +23,11 @@ defmodule Stubr do
     end
   end
 
-  defp get_args(functions) when is_list(functions),
-    do: functions |> Enum.map(&get_args(&1))
-  defp get_args({name, impl}),
+  defp put_args({name, impl}),
     do: {name, impl, get_args(impl)}
+  defp put_args(function_reps),
+    do: function_reps |> Enum.map(&put_args(&1))
+
   defp get_args({:fn, [_], [{:->, [_], [args, _]}]}),
     do: args
 
