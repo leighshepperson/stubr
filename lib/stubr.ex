85377855f7bc589651bb2cb15629afd0b13cc849
leighshepperson <pmp07ls@gmail.com>
2016-09-13 08:21:01 +0100

    Get args by paternmatching to get anonymous function definitions working for stub

--- lib/stubr.ex
+++ lib/stubr.ex
@@ -1,26 +1,34 @@
 defmodule Stubr do
   @name_generator Application.get_env(:stubr, :name_generator)
 
-  defp create_args(_, 0),
-    do: []
-  defp create_args(module, amount),
-    do: Enum.map(1..amount, &(Macro.var (:"arg#{&1}"), module))
+  defmacro stub(functions) do
 
-  defmacro stub({function_name, function_impl}) do
+     body = functions |> create_body
+
      stub_name = @name_generator.generate()
-     {function_eval, _} = function_impl |> Code.eval_quoted
-     arity = (function_eval |> :erlang.fun_info)[:arity]
-     args = create_args(:"Elixir.#{stub_name}", arity)
 
-     content = quote bind_quoted: [function_name: function_name, function_impl: Macro.escape(function_impl), args: Macro.escape(args)] do
-        def unquote(function_name)(unquote_splicing(args)) do
-           unquote(function_impl).(unquote_splicing(args))
-        end
-      end
-
-    {_, stub, _, _} = Module.create(:"Elixir.#{stub_name}", content, Macro.Env.location(__ENV__))
+    {_, stub, _, _} = Module.create(:"Elixir.#{stub_name}", body, Macro.Env.location(__ENV__))
 
     stub
   end
+
+  defp create_body(functions) do
+    functions_with_args = functions |> get_args
+
+    quote bind_quoted: [functions_with_args: Macro.escape(functions_with_args)] do
+      for {name, impl, args} <- functions_with_args do
+        def unquote(name)(unquote_splicing(args)) do
+          unquote(impl).(unquote_splicing(args))
+        end
+      end
+    end
+  end
+
+  defp get_args(functions) when is_list(functions),
+    do: functions |> Enum.map(&get_args(&1))
+  defp get_args({name, impl}),
+    do: {name, impl, get_args(impl)}
+  defp get_args({:fn, [_], [{:->, [_], [args, _]}]}),
+    do: args
 
 end
--- test/stubr_test.exs
+++ test/stubr_test.exs
@@ -3,11 +3,43 @@ defmodule StubrTest do
   doctest Stubr
   import Stubr
 
-  test "Can create a new module with a function accepting a single set of fixed parameters" do
-    stubed = stub({:good_bye, fn(1, 4) -> :canned_response end})
+  test "Creates a module with a function that executes if the arguments match a single pattern" do
+    stubed = stub([{:good_bye, fn(1, 4) -> :canned_response end}])
 
     assert stubed.module_info[:exports][:good_bye] == 2
     assert stubed.good_bye(1, 4) == :canned_response
+  end
+
+  test "Creates a module with a function defined by anonymous functions" do
+    stubed = stub([
+      {:good_bye, fn(1, 4, 9) -> :canned_response end},
+      {:good_bye, fn(1, :ok, 1) -> :canned_response_1 end},
+      {:good_bye, fn([1, 2, :ok], %{2 => [a: "b"]}, true) -> {:ok} end},
+      {:good_bye, fn(x, y, z) -> x + y + z end}
+    ])
+
+    assert stubed.module_info[:exports][:good_bye] == 3
+    assert stubed.good_bye(1, 4, 9) == :canned_response
+    assert stubed.good_bye(1, :ok, 1) == :canned_response_1
+    assert stubed.good_bye([1, 2,:ok], %{2 => [a: "b"]}, true) == {:ok}
+    assert stubed.good_bye(1, 2, 3) == 6
+  end
+
+  test "Creates a module with functions defined by anonymous functions" do
+    stubed = stub([
+      {:good_bye, fn(1, 4, 9) -> :canned_response end},
+      {:aurevoir, fn(1, :ok, 1) -> :canned_response_1 end},
+      {:hello, fn([1, 2, :ok], %{2 => [a: "b"]}, true) -> {:ok} end},
+      {:bonjour, fn(x, 3, z) -> [x, :ok, x*z] end},
+      {:bonjour, fn(x, y, z) -> x + y + z end}
+    ])
+
+    assert stubed.module_info[:exports][:good_bye] == 3
+    assert stubed.good_bye(1, 4, 9) == :canned_response
+    assert stubed.aurevoir(1, :ok, 1) == :canned_response_1
+    assert stubed.hello([1, 2,:ok], %{2 => [a: "b"]}, true) == {:ok}
+    assert stubed.bonjour(2, 3, 3) == [2, :ok, 6]
+    assert stubed.bonjour(1, 2, 3) == 6
   end
 
 end
