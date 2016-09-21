063ee44c11c2f31f9d661495572781ce0df3736c
leighshepperson <pmp07ls@gmail.com>
2016-09-21 04:30:18 +0100

    Updated to v1.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Stubr.Mixfile do
 
   def project do
     [app: :stubr,
-     version: "1.0.3",
+     version: "1.1.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
